757364e88a75e1080a2a71afab5c167fcedf85eb
unknown <dilepego3@gmail.com>
2025-01-28 14:24:55 -0300

    html e css da pagina

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,32 @@
 import { Button } from "@/components/ui/button";
 
-const PageHome = () => (<h1>bem vindo de volta</h1>);
-const PageAutenticar = () => (<Button>autenticar com suap</Button>);
+const PageHome = () => (<Button className="auth-button" >entrar</Button>);
+const PageAutenticar = () => (<Button className="auth-button" >autenticar com suap</Button>);
 const estaAutenticao = false;
 const Page = () => (estaAutenticao ? <PageHome /> : <PageAutenticar />);
 
 export default function Home() {
   return (
-    <div>
+   
+    <div className="flex flex-col h-screen">
+      {/* Header */}
+      <header className="bg-stone-600 text-white p-4">
+        <h1 className="text-lg font-semibold">coordenadores</h1>
+      </header>
+    <div className="flex-grow flex justify-center items-center">
+        {/* Quadrado com informações sobre o site */}
+        <div className="bg-white p-6 rounded-lg shadow-lg w-96 h-64">
+          <h2 className="text-xl font-semibold mb-4">Sobre o Site</h2>
+          <p className="text-gray-700 mb-4">
+            O web app de coordenadores do IFRN centraliza e otimiza a gestão acadêmica.
+          </p>
+    
+    <div className="container">
       <Page />
+        </div>
+      </div>
     </div>
-  );
+  </div>
+ );
+
 }
